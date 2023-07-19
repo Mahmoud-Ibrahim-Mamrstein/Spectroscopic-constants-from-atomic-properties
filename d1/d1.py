76b7254cad142a5b8d8247897665bd03d02364d8
Mahmoud Ibrahim <76408882+Mahmoud-Ibrahim-Mamrstein@users.noreply.github.com>
2023-07-20 01:39:31 +0300

    Update d1.py

--- d1/d1.py
+++ d1/d1.py
@@ -1,1 +1,174 @@
+#import packages
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+import numpy as np
+import sklearn as sk
+from matplotlib.backends.backend_pdf import PdfPages
+import re
+import seaborn as sns
+from matplotlib import pyplot
+import time
+import math
+from math import sqrt
+import scipy
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedKFold, LeaveOneOut
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel, Product, Matern, WhiteKernel, RBF, DotProduct, ExpSineSquared
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
+class MyGPR(GaussianProcessRegressor): #MyGPR(GaussianProcessRegressor) class specify the maximum number of iterations, tolerance, and optimizer explicitly in the sklearn.gaussian_process GaussianProcessRegressor object 
+    def __init__(self, *args, max_iter=1000000, gtol=1e-6, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._max_iter = max_iter #maximum number of iterations
+        self._gtol = gtol #tolerance
+
+    def _constrained_optimization(self, obj_func, initial_theta, bounds): 
+        if self.optimizer == "fmin_l_bfgs_b": #optmizer 
+            opt_res = scipy.optimize.minimize(obj_func, initial_theta, method="L-BFGS-B", jac=True, bounds=bounds,tol=self._gtol, options={'maxiter':self._max_iter, 'disp':True})
+            #_check_optimize_result("lbfgs", opt_res)
+            theta_opt, func_min = opt_res.x, opt_res.fun
+        elif callable(self.optimizer):
+            theta_opt, func_min = self.optimizer(obj_func, initial_theta, bounds=bounds)
+        else:
+            raise ValueError("Unknown optimizer %s." % self.optimizer)
+            
+        return theta_opt, func_min
+def load(handel,old_handel): #Load is a function that takes the handles of the two CSV files containing the full data set (including old and new data) and the data set containing the data from Liu et al. 2021 and returns multiple pandas data frames of the data as defined below
+    dfe=pd.read_csv(handel,index_col=None)
+    df1=pd.read_csv(r"/gpfs/home/maaibrahim/gpr/peridic.csv",index_col=None) #Includes information from the periodic table for each element
+    dfe= dfe.loc[:, ~dfe.columns.str.contains('^Unnamed')]
+    nul=[np.NaN]*len(dfe.Molecule)
+    for char in ['e1','e2']: #creating two columns that take in the number of electrons of the elements compromising the diatomic molecules
+        dfe[char]=nul
+    for char in df1.Symbol:
+        ind1=dfe.loc[dfe['Molecule'].str.contains(r'^'+char+r'\D')].index.values
+        ind2=dfe.loc[dfe['Molecule'].str.contains(char+r'$')].index.values
+        ind3=dfe.loc[dfe['Molecule'].str.contains(r'^'+char+r'2')].index.values
+        #print(char)
+        #print(df1[df1.Symbol==char].Period.values)
+        dfe.loc[ind1,'e1']=df1[df1.Symbol==char].NumberofElectrons.values[0]
+        dfe.loc[ind2,'e2']=df1[df1.Symbol==char].NumberofElectrons.values[0]
+        dfe.loc[ind3,'e1']=df1[df1.Symbol==char].NumberofElectrons.values[0]
+        dfe.loc[ind3,'e2']=df1[df1.Symbol==char].NumberofElectrons.values[0]
+    g=dfe
+    g['n1']=g.A1-g.e1 #number of neutrons of element 1 in a diatomic molecule
+    g['n2']=g.A2-g.e2 #number of neutrons of element 2 in a diatomic molecule
+    g.loc[g.atom2=='H','p2']=[1]*len(g.loc[g.atom2=='H']['p2'])
+    g.loc[g.atom1=='H','p1']=[1]*len(g.loc[g.atom1=='H']['p1'])
+    g.loc[g.atom2=='H','g2_lan_act']=[1]*len(g.loc[g.atom2=='H']['g2_lan_act'])
+    g.loc[g.atom1=='H','g1_lan_act']=[1]*len(g.loc[g.atom1=='H']['g1_lan_act'])
+    #the 'lan_act' extension to 'g1' and 'g2' indicates that Lanthanides and Actinides are included and both are indicated by group number 3.
+    
+    
+    g.loc[g.atom2=='D','p2']=[1]*len(g.loc[g.atom2=='D']['p2'])
+    g.loc[g.atom1=='D','p1']=[1]*len(g.loc[g.atom1=='D']['p1'])
+    g.loc[g.atom2=='T','p2']=[1]*len(g.loc[g.atom2=='T']['p2'])
+    g.loc[g.atom1=='T','p1']=[1]*len(g.loc[g.atom1=='T']['p1'])
+    g.loc[g.atom2=='D','g2']=[1]*len(g.loc[g.atom2=='D']['g2'])
+    g.loc[g.atom1=='D','g1']=[1]*len(g.loc[g.atom1=='D']['g1'])
+    g.loc[g.atom2=='T','g2']=[1]*len(g.loc[g.atom2=='T']['g2'])
+    g.loc[g.atom1=='T','g1']=[1]*len(g.loc[g.atom1=='T']['g1'])
+    g.loc[g.atom2=='D','g2_lan_act']=[1]*len(g.loc[g.atom2=='D']['g2_lan_act'])  
+    g.loc[g.atom1=='D','g1_lan_act']=[1]*len(g.loc[g.atom1=='D']['g1_lan_act'])
+    g.loc[g.atom2=='T','g2_lan_act']=[1]*len(g.loc[g.atom2=='T']['g2_lan_act'])
+    g.loc[g.atom1=='T','g1_lan_act']=[1]*len(g.loc[g.atom1=='T']['g1_lan_act'])
+    g.loc[g.atom2=='D','g2_lan_act_iso']=[0]*len(g.loc[g.atom2=='D']['g2_lan_act_iso']) #including the isotopic character in some atoms through the groups of the perspective elements  
+    g.loc[g.atom1=='D','g1_lan_act_iso']=[0]*len(g.loc[g.atom1=='D']['g1_lan_act_iso']) #including the isotopic character in some atoms through the groups of the perspective elements 
+    g.loc[g.atom2=='T','g2_lan_act_iso']=[-1]*len(g.loc[g.atom2=='T']['g2_lan_act_iso']) #including the isotopic character in some atoms through the groups of the perspective elements 
+    g.loc[g.atom1=='T','g1_lan_act_iso']=[-1]*len(g.loc[g.atom1=='T']['g1_lan_act_iso']) #including the isotopic character in some atoms through the groups of the perspective elements 
+    g.loc[g.atom2=='H','g2_lan_act_iso']=[1]*len(g.loc[g.atom2=='H']['g2_lan_act_iso']) #including the isotopic character in some atoms through the groups of the perspective elements 
+    g.loc[g.atom1=='H','g1_lan_act_iso']=[1]*len(g.loc[g.atom1=='H']['g1_lan_act_iso']) #including the isotopic character in some atoms through the groups of the perspective elements
+    #creating different variations of features and targets 
+    g['sum_p']=g['p1']+g['p2']
+    g['sum_g']=g.g1_lan_act+g.g2_lan_act
+    g['diff_p']=abs(g['p1']-g['p2'])
+    g['diff_g']=abs(g['g1_lan_act']-g['g2_lan_act'])
+    g['product_p']=g['p1']*g['p2']
+    g['product_g']=g['g1_lan_act']*g['g2_lan_act']
+    g['Reduced_g']=(g.g1_lan_act*g.g2_lan_act)/(g.g1_lan_act+g.g2_lan_act)
+    g['Reduced_p']=(g.p1*g.p2)/(g.p1+g.p2)
+    g['g_average']=(g.g1+g.g2)/2
+    g['g_average_lan_act']=(g.g1_lan_act+g.g2_lan_act)/2
+    g['g_average_lan_act_iso']=(g.g1_lan_act_iso+g.g2_lan_act_iso)/2
+    g['Re (\AA)^-1']=1/((g['Re (\AA)']))
+    g['Re (au)']=((g['Re (\AA)'])*1.8897259885789) #unit conversion
+    g['Re (au)^-1']=1/((g['Re (\AA)'])*1.8897259885789)
+    g['Re (au)^2']=((g['Re (\AA)'])*1.8897259885789)**2
+    g['Re (au)^-2']=1/g['Re (au)^2']
+    g['4*(np.pi**2)*Re (au)']=(4*(np.pi**2)*(g['Re (\AA)'])*1.8897259885789)
+    g['4*(np.pi**2)*Re (au)^-1']=1/(4*(np.pi**2)*(g['Re (\AA)'])*1.8897259885789)
+    g['omega_e (au)^-2']=1/((g['omega_e (cm^{-1})']*0.0000046)**2)
+    g['4*(np.pi**2)*omega_e (au)^-2']=1/((2*(np.pi)*g['omega_e (cm^{-1})']*0.0000046)**2)
+    g['omega_e (au)']=(g['omega_e (cm^{-1})']*(0.0000046)) #unit conversion
+    g['omega_e (au)^2']=((g['omega_e (cm^{-1})']*0.0000046)**2)
+    g['4*(np.pi**2)*omega_e (au)^2']=4*(np.pi**2)*((g['omega_e (cm^{-1})']*(0.0000046))**2)
+    g['K']=(4*(np.pi**2)*((g['omega_e (cm^{-1})'])**2)*g['Reduced mass']) # 'K' indicates the force constant 
+    g['sqrt(K)']=np.sqrt(4*(np.pi**2)*((g['omega_e (cm^{-1})'])**2)*g['Reduced mass'])
+    g['4*(np.pi**2)*omega_e (au)']=4*(np.pi**2)*((g['omega_e (cm^{-1})']*(0.0000046)))
+    g['4*(np.pi**2)*Re (au)*omega_e (au)^2']=g['4*(np.pi**2)*Re (au)']*((g['omega_e (cm^{-1})']*0.0000046)**2)
+    g['ve1']=g['g1_lan_act'] #indicating valence electrons of element 1
+    g['ve2']=g['g2_lan_act'] #indicating valence electrons of element 2
+    g['log(D_e)/(R_e^3*Z_1*Z_2)']=np.log((g["D0 (eV)"]*0.037)/((g["Re (au)"]**3)*g.e1*g.e2))
+    #redfinining valence electrons 
+    g.loc[g.g2_lan_act==18,'ve2']=[0]*len(g.loc[g.g2_lan_act==18]['g2_lan_act']) 
+    g.loc[g.g1_lan_act==18,'ve1']=[0]*len(g.loc[g.g1_lan_act==18]['g1_lan_act'])
+    g.loc[g.g2_lan_act==17,'ve2']=[7]*len(g.loc[g.g2_lan_act==17]['g2_lan_act'])
+    g.loc[g.g1_lan_act==17,'ve1']=[7]*len(g.loc[g.g1_lan_act==17]['g1_lan_act'])
+    g.loc[g.g2_lan_act==16,'ve2']=[6]*len(g.loc[g.g2_lan_act==16]['g2_lan_act'])
+    g.loc[g.g1_lan_act==16,'ve1']=[6]*len(g.loc[g.g1_lan_act==16]['g1_lan_act'])
+    g.loc[g.g2_lan_act==15,'ve2']=[5]*len(g.loc[g.g2_lan_act==15]['g2_lan_act'])
+    g.loc[g.g1_lan_act==15,'ve1']=[5]*len(g.loc[g.g1_lan_act==15]['g1_lan_act'])
+    g.loc[g.g2_lan_act==14,'ve2']=[4]*len(g.loc[g.g2_lan_act==14]['g2_lan_act'])
+    g.loc[g.g1_lan_act==14,'ve1']=[4]*len(g.loc[g.g1_lan_act==14]['g1_lan_act'])
+    g.loc[g.g2_lan_act==13,'ve2']=[3]*len(g.loc[g.g2_lan_act==13]['g2_lan_act'])
+    g.loc[g.g1_lan_act==13,'ve1']=[3]*len(g.loc[g.g1_lan_act==13]['g1_lan_act'])
+    g.loc[g.type2=='Transition Metal','ve2']=[2]*len(g.loc[g.type2=='Transition Metal']['g2_lan_act'])
+    g.loc[g.type1=='Transition Metal','ve1']=[2]*len(g.loc[g.type1=='Transition Metal']['g1_lan_act'])
+    g= g.loc[:, ~g.columns.str.contains('^Unnamed')]
+    g_dict=g.to_dict(orient='list')
+    old=pd.read_csv(old_handel) #loading data from the Liu et al. 2021 paper
+    old=old[old["Te (cm^{-1})"]==0]
+    old.drop_duplicates(inplace=True)
+    #creating different pandas dataframes for different purposes
+    gr=g[g["Re (\AA)"].isna()==False] #gr only contains molecules that have R_e available 
+    gw=gr[gr["omega_e (cm^{-1})"].isna()==False] #gw only contains molecules that have R_e and omega_e available
+    g_new=g.loc[g['Molecule'].isin(old.Molecule)==False] #g_new contains only new data
+    g_old=g.loc[g['Molecule'].isin(old.Molecule)] #g_new contains only old data from liu et al., 2021
+    gr_old=g_old[g_old["Re (\AA)"].isna()==False] #gr_old only contains molecules that have R_e available from liu et al., 2021
+    gw_old=gr_old[gr_old["omega_e (cm^{-1})"].isna()==False] #gw_old only contains molecules that have R_e and omega_e available from liu et al., 2021
+    gr_new=g_new[g_new["Re (\AA)"].isna()==False] #gr_new only contains new molecules that have R_e available
+    gw_new=gr_new[gr_new["omega_e (cm^{-1})"].isna()==False] #gw_new only contains new molecules that have R_e and omega_e available
+    
+    # permuting the properties of atoms 1 and 2 in the diatomic molecules as described in Liu et al., 2021 and in the manuscript, to create expanded data frames containing both A-B and B-A molecules
+    reverse=['A1','A2','g1','g2','p1','p2','g1iso','g2iso','g1_lan_act','g2_lan_act','g1_lan_act_iso','g2_lan_act_iso','atom1','atom2','type1','type2','e1','e2','ve1','ve2']
+    for key,value in g_dict.items():
+        if key in reverse:
+            continue 
+        else:
+            g_dict[key]=value+value
+            #=g_dict[key].append(g_dict[key])
+    s=0        
+    for i in range(len(reverse)):
+            if s==len(reverse):
+                break
+            A=g_dict[reverse[s]]+g_dict[reverse[s+1]]
+            B=g_dict[reverse[s+1]]+g_dict[reverse[s]]
+            g_dict[reverse[s]]=A
+            g_dict[reverse[s+1]]=B
+            s=s+2
+    #the extension '-expand' indicates the inclusion of both A-B and B-A variations of the diatomic molecule in the dataframe 
+    g_expand=pd.DataFrame.from_dict(g_dict, orient='columns')
+    g_expand.drop_duplicates(subset=['A1','A2','g1','g2','p1','p2','g1iso','g2iso','g1_lan_act','g2_lan_act','g1_lan_act_iso','g2_lan_act_iso','atom1','atom2','type1','type2','ve1','ve2'], keep='first', inplace=True, ignore_index=False)
+    gr_expand=g_expand[g_expand["Re (\AA)"].isna()==False]
+    gw_expand=gr_expand[gr_expand["omega_e (cm^{-1})"].isna()==False]
+
+    g_new_expand=g_expand.loc[g_expand['Molecule'].isin(old.Molecule)==False]
+    g_old_expand=g_expand.loc[g_expand['Molecule'].isin(old.Molecule)]
+    gr_old_expand=g_old_expand[g_old_expand["Re (\AA)"].isna()==False]
+    gw_old_expand=gr_old_expand[gr_old_expand["omega_e (cm^{-1})"].isna()==False]
+    gr_new_expand=g_new_expand[g_new_expand["Re (\AA)"].isna()==False]
+    gw_new_expand=gr_new_expand[gr_new_expand["omega_e (cm^{-1})"].isna()==False]
+    return g,gr,gw, g_old, g_new, gr_old, gw_old, gr_new, gw_new, g_expand, gr_expand, gw_expand, g_old_expand, g_new_expand, gr_old_expand, gw_old_expand, gr_new_expand, gw_new_expand
+  
